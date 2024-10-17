0f1d805a03fae32d73a811e627b75e7126854e25
ForSureNotReaper <szkolagithub@gmail.com>
2024-10-17 12:23:11 +0200

    2
    2

--- GRaWKAmien/Program.cs
+++ GRaWKAmien/Program.cs
@@ -2,6 +2,23 @@
 {
     static void Main(String[] args)
     {
-       
-    }
+        Random rnd = new Random();
+        int wybor_komputera = rnd.Next(3);
+        int wybor = int.Parse(Console.ReadLine());
+        if (wybor == 0)
+        {
+            Console.WriteLine("Wybierz 1, 2 albo 3.");
+            wybor = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Wybierz (1) papier, (2) kamien, (3) nozyce, lub (0) zeby wyjsc");
+        switch (wybor)
+        {
+            case 1:
+                break;
+            {
+            
+            }
+            
+        }
+    }        
 }
